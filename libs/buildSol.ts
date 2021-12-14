81cb47cd10e85e7828aaff9b76ffd6340ae67426
Kate-AC <alice.catharsis.kate@gmail.com>
2021-12-14 23:13:22 +0900

    support import

--- libs/buildSol.ts
+++ libs/buildSol.ts
@@ -4,6 +4,7 @@ import exec from 'child_process';
 import ContractParser from './ContractParser';
 import deployContract from './deployContract';
 import listEthAccount from './listEthAccount';
+const solc = require('solc');
 
 const searchRecursive = async (filePath: string, foundFiles: string[] = []): Promise<string[]> => {
   const dirents: fs.Dirent[] = await fs.promises.readdir(filePath, {withFileTypes: true});
@@ -31,47 +32,61 @@ const pickOutFineName = (path: string) => {
 
 (async () => {
   const solFiles: string[] = await searchRecursive('src');
-
+  const imports: any = [];
+  solFiles.forEach((path: string) => {
+    imports[path] = fs.readFileSync(path, 'utf8');
+  });
+  const findImports = (path: string) => {
+    return {
+      contents: imports['src/' + path]
+    }
+  }
+  
   solFiles.forEach((solFile: string) => {
-    exec.exec('solc --bin --abi ' + solFile, (error, stdout) => {
-      if (error !== null) {
-        console.log(error);
-        return;
+    const sourceCode = fs.readFileSync(solFile, 'utf8');
+
+    const list = solFile.split('/');
+    const fileName = list[list.length - 1].slice(0, -4);
+
+    const input = {
+      language: 'Solidity',
+      sources: { main: { content: sourceCode } },
+      settings: { outputSelection: { '*': { '*': ['abi', 'evm.bytecode'] } } },
+    }
+    const output = solc.compile(JSON.stringify(input), { import: findImports });
+    const contract = JSON.parse(output).contracts.main[fileName];
+
+    (async () => {
+      const list: string[] = await listEthAccount();
+      const contractAddress: string = await deployContract(
+        list[0],
+        contract.evm.bytecode.object,
+        JSON.stringify(contract.abi)
+      );
+
+      const baseFileName = pickOutFineName(solFile);
+      const fineName: string = baseFileName + 'Contract.ts';
+      const varName: string = baseFileName.charAt(0).toLowerCase() + baseFileName.slice(1) + 'Contract';
+      const json: any = {
+        address: contractAddress,
+        abi: contract.abi,
       }
-      const contractParser = new ContractParser(stdout);
-      (async () => {
-        const list: string[] = await listEthAccount();
-        const contractAddress: string = await deployContract(
-          list[0],
-          contractParser.getBinary(),
-          contractParser.getAbi()
-        );
 
-        const baseFileName = pickOutFineName(solFile);
-        const fineName: string = baseFileName + 'Contract.ts';
-        const varName: string = baseFileName.charAt(0).toLowerCase() + baseFileName.slice(1) + 'Contract';
-        const json: any = {
-          address: contractAddress,
-          abi: JSON.parse(contractParser.getAbi()),
-        }
-
-        fs.writeFileSync(
-          `./built/${fineName}`,
-          `import { ContractJsonType } from '../libs/contractJsonType';
+      fs.writeFileSync(
+        `./built/${fineName}`,
+        `import { ContractJsonType } from '../libs/contractJsonType';
 
 const ${varName}: ContractJsonType = ${JSON.stringify(json, null, 2)}
 
 export default ${varName};
-          `
-        );
-  
-        console.log(`
-          [ ${solFile} ]
-          ContractId: ${contractAddress}
-          ABI: ${contractParser.getAbi()}
+        `
+      );
 
-        `);
-      })();
-    });
+      console.log(`
+        [ ${solFile} ]
+        ContractId: ${contractAddress}
+        ABI: ${JSON.stringify(contract.abi)}
+      `);
+    })();
   })
 })();
--- src/HelloWorld.sol
+++ src/HelloWorld.sol
@@ -1,11 +1,12 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract HelloWorlda {
-  function getSender() public returns (address) {
+contract HelloWorld {
+  function getSender() public view returns (address) {
     return msg.sender;
   }
 
-  function getBalance() public returns (uint256) {
+  function getBalance() public view returns (uint256) {
     return address(msg.sender).balance;
   }
 }
